da39555caaf1ddc0e5c0dc9c7ee4ea78163ad042
Baris Unluer <unluerbaris@gmail.com>
2020-08-21 22:50:24 +0900

    mover

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    float gravitySpeed = 0.25f;
+    float xSpeed = 32f;
+
+    void Update()
+    {
+        transform.position -= transform.up * gravitySpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * xSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * xSpeed * Time.deltaTime;
+        }
+    }
+}
