0e2567f66219e473719d3eb0726457c43b3f6455
Baris Unluer <unluerbaris@gmail.com>
2020-08-23 22:32:08 +0900

    target fps

---
+++ Assets/Scripts/TargetFPS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFPS : MonoBehaviour
+{
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+    }
+}
