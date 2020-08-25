2ab0b2c2920c4c7c31e538485ef347de6ed58705
Baris Unluer <unluerbaris@gmail.com>
2020-08-25 15:19:01 +0900

    fill the board with random characters

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,14 +8,18 @@ public class Board : MonoBehaviour
     [SerializeField] int height;
     [SerializeField] int borderSize = 1;
     [SerializeField] GameObject tilePrefab;
+    [SerializeField] GameObject[] characterPrefabs;
 
     Tile[,] tiles;
+    Character[,] characters; 
 
     private void Start()
     {
         tiles = new Tile[width, height];
+        characters = new Character[width, height];
         SetupTiles();
         SetupCamera();
+        FillRandom();
     }
 
     private void SetupTiles()
@@ -36,9 +40,51 @@ public class Board : MonoBehaviour
     private void SetupCamera()
     {
         Camera.main.transform.position = new Vector3((float)(width - 1)/2f, (float)(height - 1)/2f, -10f);
+
         float aspectRatio = (float)Screen.width / (float)Screen.height;
         float verticalSize = (float)height / 2f + (float)borderSize;
         float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
+
         Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+    private GameObject GetRandomCharacter()
+    {
+        int randomIndex = Random.Range(0, characterPrefabs.Length);
+
+        if (characterPrefabs[randomIndex] == null)
+        {
+            Debug.LogWarning($"Index {randomIndex} does not contain a valid prefab!");
+        }
+        return characterPrefabs[randomIndex];
+    }
+
+    private void PlaceCharacter(Character character, int x, int y)
+    {
+        if (character == null)
+        {
+            Debug.LogWarning("Invalid character!");
+            return;
+        }
+
+        character.transform.position = new Vector2(x, y);
+        character.transform.rotation = Quaternion.identity;
+        character.SetCoord(x, y);
+    }
+
+    private void FillRandom()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject randomCharacter = Instantiate(GetRandomCharacter(), Vector2.zero, Quaternion.identity) as GameObject;
+
+                if (randomCharacter != null)
+                {
+                    PlaceCharacter(randomCharacter.GetComponent<Character>(), i, j);
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] int xIndex;
+    [SerializeField] int yIndex;
+
+    public void SetCoord(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
+}
