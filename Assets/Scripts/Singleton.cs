a5463a8ed4c82e4c437b12ab9d7cf1ed6c5ec5d2
Baris Unluer <unluerbaris@gmail.com>
2020-09-02 10:36:08 +0900

    singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Declare a generic type class
+public class Singleton<T> : MonoBehaviour where T: MonoBehaviour
+{
+    static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            //transform.parent == null;
+
+            // Toggle this if needed
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
